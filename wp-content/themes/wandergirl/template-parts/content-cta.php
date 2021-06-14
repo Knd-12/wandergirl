2abf379a79bfd55130b29fadc4a3b9a335b5ce80
kim d'sa <kim1296@live.com>
2021-06-14 16:14:33 -0700

    content-cta

--- wp-content/themes/wandergirl/template-parts/content-cta.php
+++ wp-content/themes/wandergirl/template-parts/content-cta.php
@@ -2,7 +2,7 @@
 
     <div class="caption">
         <h3 class="text-center mb-3 h2">Want to Work With Me?</h3>
-        <p class="text-center mb-5">I am looking for fun, enthusastic individuals who love to travel.</p>
+        <p class="text-center mb-5">I am looking for fun, enthusiastic individuals who love to travel.</p>
         <div class="text-center"><a class="my-cta-button" href="/contact/">Drop Me A Message</a></div>
     </div>
 
